9e9828225b42371ac10fe4b79bf71cf71e207d4a
Mburiah <bykieswagix5@gmail.com>
2020-02-03 11:48:02 +0300

    code to make pages work

--- app/src/main/java/com/example/rickmorty/ListActivity.java
+++ app/src/main/java/com/example/rickmorty/ListActivity.java
@@ -1,4 +1,20 @@
 package com.example.rickmorty;
 
-public class ListActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.BindView;
+
+public class ListActivity extends AppCompatActivity{
+
 }
--- app/src/main/java/com/example/rickmorty/MainActivity.java
+++ app/src/main/java/com/example/rickmorty/MainActivity.java
@@ -3,13 +3,35 @@ package com.example.rickmorty;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
+import butterknife.ButterKnife;
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.viewMoreButton) Button mViewMoreButton;
+    @BindView(R.id.formEdittext) EditText mFormEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mViewMoreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String form = mFormEditText.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this,ListActivity.class);
+                intent.putExtra(form, form);
+                startActivity(intent);
+            }
+        });
 
     }
 }

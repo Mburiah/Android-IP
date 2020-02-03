76dd1807974ed89443c827737cc73d5faf9d704e
Mburiah <bykieswagix5@gmail.com>
2020-02-03 15:47:28 +0300

    logic to display characters

--- app/src/main/java/com/example/rickmorty/ListActivity.java
+++ app/src/main/java/com/example/rickmorty/ListActivity.java
@@ -16,5 +16,28 @@ import butterknife.ButterKnife;
 import butterknife.BindView;
 
 public class ListActivity extends AppCompatActivity{
+    @BindView(R.id.charactersTextView) TextView mCharactersTextView;
+    @BindView(R.id.listView1) ListView mListView;
+
+    private String[]characters = new String[]{"Rick Sanchez", "Morty Smith", "Jerry Smith", "Unity", "Summer Smith", "Beth Smith", "Mr. Meeseeks", "Elom Tusk" };
+
+    @Override
+    protected void  onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        ButterKnife.bind(this);
+
+        ArrayAdapter adapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1, characters);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String characters = ((TextView)view).getText().toString();
+                Toast.makeText(ListActivity.this, characters,Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
 
 }

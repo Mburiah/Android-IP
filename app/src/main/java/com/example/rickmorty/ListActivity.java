8d06465e83efd54f3113bcf7e2d39da7e57adc72
Mburiah <bykieswagix5@gmail.com>
2020-02-03 16:32:18 +0300

    linking view location button to next activity

--- app/src/main/java/com/example/rickmorty/ListActivity.java
+++ app/src/main/java/com/example/rickmorty/ListActivity.java
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +18,7 @@ import butterknife.BindView;
 public class ListActivity extends AppCompatActivity{
     @BindView(R.id.charactersTextView) TextView mCharactersTextView;
     @BindView(R.id.listView1) ListView mListView;
+    @BindView(R.id.locationButton) Button mLocationButton;
 
     private String[]characters = new String[]{"Rick Sanchez", "Morty Smith", "Jerry Smith", "Unity", "Summer Smith", "Beth Smith", "Mr. Meeseeks", "Elom Tusk" };
 
@@ -27,9 +28,6 @@ public class ListActivity extends AppCompatActivity{
         setContentView(R.layout.activity_list);
         ButterKnife.bind(this);
 
-        ArrayAdapter adapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1, characters);
-        mListView.setAdapter(adapter);
-
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -37,6 +35,18 @@ public class ListActivity extends AppCompatActivity{
                 Toast.makeText(ListActivity.this, characters,Toast.LENGTH_LONG).show();
             }
         });
+
+        ArrayAdapter adapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1, characters);
+        mListView.setAdapter(adapter);
+
+        mLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListActivity.this,NextActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 

1fdfc84ed49a8f9acbcda1916b90683284746222
Mburiah <bykieswagix5@gmail.com>
2020-02-14 11:13:28 +0300

    network package containing API interface class and Client class

--- app/src/main/java/com/example/rickmorty/network/RickandmortyApi.java
+++ app/src/main/java/com/example/rickmorty/network/RickandmortyApi.java
@@ -1,4 +1,16 @@
 package com.example.rickmorty.network;
 
-public class RickandmortyApi {
+import com.example.rickmorty.models.RickandmortyApiInformationSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RickandmortyApi {
+    @GET("information/search")
+    Call<RickandmortyApiInformationSearchResponse> getInformation(
+            @Query("characters") String characters,
+            @Query("locations") String locations,
+            @Query("episodes") String episodes
+    );
 }
--- app/src/main/java/com/example/rickmorty/network/RickandmortyClient.java
+++ app/src/main/java/com/example/rickmorty/network/RickandmortyClient.java
@@ -1,4 +1,36 @@
 package com.example.rickmorty.network;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RickandmortyClient {
+    public static final String BASE_URL = "https://rickandmortyapi.com/api/";
+
+    private static Retrofit retrofit = null;
+     public static RickandmortyApi getClient(){
+         if (retrofit == null){
+             OkHttpClient okHttpClient= new OkHttpClient.Builder()
+                     .addInterceptor(new Interceptor() {
+                         @Override
+                         public Response intercept(Chain chain) throws IOException {
+                             Request newRequest = chain.request().newBuilder()
+                                     .build();
+                             return chain.proceed(newRequest);
+                         }
+                     })
+                     .build();
+             retrofit = new Retrofit.Builder()
+                     .baseUrl(BASE_URL)
+                     .client(okHttpClient)
+                     .addConverterFactory(GsonConverterFactory.create())
+                     .build();
+         }
+         return retrofit.create(RickandmortyApi.class);
+     }
 }

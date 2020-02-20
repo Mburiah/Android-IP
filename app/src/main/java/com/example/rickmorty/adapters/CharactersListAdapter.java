0b37e0c615b317c778fabdd99616f42f21ec9fed
Mburiah <bykieswagix5@gmail.com>
2020-02-20 17:11:20 +0300

    imageview, picasso, intent

--- app/src/main/java/com/example/rickmorty/adapters/CharactersListAdapter.java
+++ app/src/main/java/com/example/rickmorty/adapters/CharactersListAdapter.java
@@ -1,18 +1,26 @@
 package com.example.rickmorty.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.example.rickmorty.adapters.CharactersListAdapter;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.rickmorty.R;
 import com.example.rickmorty.models.characters.Result;
+import com.example.rickmorty.ui.sanchezmainModel.CharacterDetailActivity;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -28,6 +36,7 @@ public class CharactersListAdapter extends RecyclerView.Adapter<CharactersListAd
     public CharactersListAdapter(Context context, List<Result> characters) {
         mContext = context;
         results = characters;
+
     }
 
     @Override
@@ -49,16 +58,16 @@ public class CharactersListAdapter extends RecyclerView.Adapter<CharactersListAd
         return results.size();
     }
 
-    public class CharactersViewHolder extends RecyclerView.ViewHolder{
-        @BindView(R.id.IDTextView) TextView mID;
+    public class CharactersViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         @BindView(R.id.characterNameTextView) TextView mName;
         @BindView(R.id.statusTextView) TextView mStatus;
         @BindView(R.id.speciesTextView) TextView mSpecies;
-        @BindView(R.id.typeTextView) TextView mType;
+        @BindView(R.id.imgView) ImageView mImage;
         @BindView(R.id.genderTextView) TextView mGender;
-        @BindView(R.id.originTextView) TextView Origin;
-        @BindView(R.id.locationView) TextView mLocation;
-        @BindView(R.id.imageTextView) TextView mImage;
+//        @BindView(R.id.originTextView) TextView Origin;
+//        @BindView(R.id.IDTextView) TextView mID;
+//        @BindView(R.id.locationView) TextView mLocation;
+//        @BindView(R.id.typeTextView) TextView mType;
 
 //        private Context context;
 
@@ -66,20 +75,35 @@ public class CharactersListAdapter extends RecyclerView.Adapter<CharactersListAd
             super(itemView);
             ButterKnife.bind(this, itemView);
             mContext = itemView.getContext();
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v){
+            int itemPosition = getLayoutPosition();
+
+            Intent intent= new Intent(mContext, CharacterDetailActivity.class);
+            intent.putExtra("position", itemPosition);
+            intent.putExtra("characters", Parcels.wrap(results));
+            mContext.startActivity(intent);
         }
 
         public void bindCharacter (Result characters){
             Log.d(TAG, "bindCharacter: bind char...");
-            mID.setText(characters.getName());
 
-            mName.setText(characters.getName());
-            mStatus.setText(characters.getStatus());
-            mSpecies.setText(characters.getStatus());
-            mType.setText(characters.getType());
-            mGender.setText(characters.getGender());
-            Origin.setText(characters.getName());
-            mLocation.setText(characters.getName());
-            mImage.setText(characters.getImage());
+            Picasso.get().load(characters.getImage()).into(mImage);
+
+//            mID.setText(characters.getId());
+            mName.setText("Name:" + " " + characters.getName());
+            mStatus.setText("Status:" + " " + characters.getStatus());
+            mSpecies.setText("Species:" + " " +characters.getSpecies());
+//            mType.setText(characters.getType());
+            mGender.setText("Gender:" + " " + characters.getGender());
+//            Origin.setText(characters.getName());
+//            mLocation.setText(characters.getName());
+            //mImage.setImage(characters.getImage());
+
+
         }
 
     }

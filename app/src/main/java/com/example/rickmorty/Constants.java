799bf704d3f43e93f732cac7d3ce94f17e72c409
Mburiah <bykieswagix5@gmail.com>
2020-02-24 21:50:18 +0300

    changed location of constants

---
+++ app/src/main/java/com/example/rickmorty/Constants.java
@@ -0,0 +1,7 @@
+package com.example.rickmorty;
+
+public class Constants {
+    public static final String PREFERENCES_CHARACTER_KEY = "character";
+    public static final String FIREBASE_CHILD_SEARCHED_CHARACTER = "searchedCharacter";
+    public static final String FIREBASE_CHILD_CHARACTERS = "characters";
+}

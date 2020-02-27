ea33039f3ae11ecb29c8578dd9989582f7aeddcb
Mburiah <bykieswagix5@gmail.com>
2020-02-27 21:06:35 +0300

    google token

--- app/src/main/java/com/example/rickmorty/Constants.java
+++ app/src/main/java/com/example/rickmorty/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String FIREBASE_CHILD_SEARCHED_CHARACTER = "searchedCharacter";
     public static final String FIREBASE_CHILD_CHARACTERS = "characters";
     public static final String FIREBASE_QUERY_INDEX = "index";
+    public static final String API_KEY = "AIzaSyB4s7G30dgg9KVnQiZFnXLMpSf__jcvws0";
 }
